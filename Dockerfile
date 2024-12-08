cbf02310e0ddc3c0d1c91bad770fa33601d0c44f
Zildiray Alfirli <zildirayalfirli@gmail.com>
2024-12-08 20:46:31 +0700

    add

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM node:16
 
 WORKDIR /app
-ENV PORT 8080
-ENV HOST 0.0.0.0
+
+COPY package*.json ./
+
+RUN npm install
 
 COPY . .
-RUN npm install
+
 EXPOSE 8080
+
 CMD [ "npm", "run", "dev"]
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { loadModel } from './services/loadModel.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 (async () => {
     try {
